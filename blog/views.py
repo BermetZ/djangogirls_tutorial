5ea5eaeb987db461402e6b399df12c5c863a2f93
BermetZ <zhumakadyrkyzy.b@gmail.com>
2019-03-26 12:32:24 +0600

    Converted some functions into classes.

--- blog/views.py
+++ blog/views.py
@@ -2,27 +2,49 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.views.generic import ListView, CreateView
+from django.urls import reverse
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+# def post_list(request):
+#     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#     return render(request, 'blog/post_list.html', {'posts': posts})
+
+class PostList(ListView):
+    template_name = 'blog/post_list.html'
+    model = Post
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
+# def post_new(request):
+#     if request.method == "POST":
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.published_date = timezone.now()
+#             post.save()
+#             return redirect('post_detail', pk=post.pk)
+#     else:
+#         form = PostForm()
+#     return render(request, 'blog/post_edit.html', {'form': form})
+
+class PostCreate(CreateView):
+    template_name = 'blog/post_edit.html'
+    form_class = PostForm
+    model = Post
+
+    def form_valid(self, form):
         if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+            form.author = self.request.user
+            form.published_date = timezone.now()
+            form.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('post_detail', kwargs={'pk': self.object.id})
+
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
